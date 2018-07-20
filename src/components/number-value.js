d83d03817e6efa55e545589216cf28b75e70ee9e
Conor Dockry <conordockry@gmail.com>
2018-07-20 11:14:03 -0400

    add max/min props to NumberValue

--- src/components/number-value.js
+++ src/components/number-value.js
@@ -1,10 +1,15 @@
 import AnyValue from './any-value'
 
 class NumberValue extends AnyValue {
-  constructor(...args) {
-    super(...args, 0)
-    this.define('increment', (v, n = 1) => v + n)
-    this.define('decrement', (v, n = 1) => v - n)
+  static defaultProps = {
+    max: Number.MAX_SAFE_INTEGER,
+    min: Number.MIN_SAFE_INTEGER,
+  }
+  constructor(props, ...rest) {
+    super(props, ...rest, 0)
+    const { max, min } = props
+    this.define('increment', (v, n = 1) => Math.min(v + n, max))
+    this.define('decrement', (v, n = 1) => Math.max(v - n, min))
   }
 }
 
