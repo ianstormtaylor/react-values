0c283aa6c89c3989b433a1ba7fb2f84890491649
Ian Storm Taylor <ian@ianstormtaylor.com>
2018-11-11 11:40:47 -0800

    expose store and value to connected values

--- src/utils/create-factory.js
+++ src/utils/create-factory.js
@@ -5,6 +5,10 @@ export default function createFactory(Store, Value) {
     const store = new Store(initial, props)
 
     class Connected extends React.Component {
+      static get value() {
+        return store.value
+      }
+
       render() {
         return <Value {...this.props} store={store} />
       }
@@ -14,6 +18,7 @@ export default function createFactory(Store, Value) {
       Connected[k] = store.transforms[k]
     }
 
+    Connected.store = store
     Connected.displayName = `Connected${Value.displayName}`
     return Connected
   }
